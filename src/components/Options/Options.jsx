1e31e2c3be1bc38d901a10fde11dce05df2ff434
StIgorek <elitelifegroup@gmail.com>
2024-08-19 20:28:16 +0300

    add btn style

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -3,10 +3,18 @@ import css from "../Options/Options.module.css";
 export default function Options({ onUpdate, onReset }) {
   return (
     <div className={css.list}>
-      <button onClick={() => onUpdate("good")}>Good</button>
-      <button onClick={() => onUpdate("neutral")}>Neutral</button>
-      <button onClick={() => onUpdate("bad")}>Bad</button>
-      <button onClick={onReset}>Reset</button>
+      <button className={css.btn} onClick={() => onUpdate("good")}>
+        Good
+      </button>
+      <button className={css.btn} onClick={() => onUpdate("neutral")}>
+        Neutral
+      </button>
+      <button className={css.btn} onClick={() => onUpdate("bad")}>
+        Bad
+      </button>
+      <button className={css.btn} onClick={onReset}>
+        Reset
+      </button>
     </div>
   );
 }
